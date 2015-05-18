6992eb9c99d614d8c70bb0ed6db8d77ded9c5f18
Tim Caswell <tim@creationix.com>
2015-05-18 12:18:22 -0500

    Use borrowed values instead of copied values

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 
-#[derive(Debug,Copy,Clone)]
-enum Raw {
+#[derive(Debug)]
+enum Value {
   // Numbers in this language can be simple integers or rational.
   // They are the same to the user and automatically switch to the best
   // type on every calculation.
   Integer(i64),
   Rational(i64, i64),
+  String(String)
 }
-
-type Value = Result<Raw, &'static str>;
+type Error = &'static str;
 
 // Calculate the greatest common divisor using Euclid's algorithm.
 fn gcd(mut a: i64, mut b: i64) -> i64 {
@@ -23,7 +23,7 @@ fn gcd(mut a: i64, mut b: i64) -> i64 {
 
 #[inline(always)]
 fn new_integer(n: i64) -> Value {
-    Ok(Raw::Integer(n))
+    Value::Integer(n)
 }
 
 // This normalizes inputs and sometimes even converts to integers.
@@ -36,10 +36,10 @@ fn new_rational(mut n: i64, mut d: i64) -> Value {
       d = -d;
     }
     if d == 1 {
-        Ok(Raw::Integer(n))
+        Value::Integer(n)
     }
     else {
-        Ok(Raw::Rational(n, d))
+        Value::Rational(n, d)
     }
 }
 
@@ -56,47 +56,51 @@ fn rational_add(n1: i64, d1: i64, n2: i64, d2: i64) -> Value {
   }
 }
 
-fn add(left: Value, right: Value) -> Value {
-    match (try!(left), try!(right)) {
-        (Raw::Integer(a), Raw::Integer(b)) => new_integer(a + b),
-        (Raw::Rational(n, d), Raw::Integer(b)) => rational_add(n, d, b, 1),
-        (Raw::Integer(a), Raw::Rational(n, d)) => rational_add(a, 1, n, d),
-        (Raw::Rational(n1, d1), Raw::Rational(n2, d2)) => rational_add(n1, d1, n2, d2)
+fn add(left: &Value, right: &Value) -> Result<Value, Error> {
+    match (left, right) {
+        (&Value::Integer(a), &Value::Integer(b)) => Ok(new_integer(a + b)),
+        (&Value::Rational(n, d), &Value::Integer(b)) => Ok(rational_add(n, d, b, 1)),
+        (&Value::Integer(a), &Value::Rational(n, d)) => Ok(rational_add(a, 1, n, d)),
+        (&Value::Rational(n1, d1), &Value::Rational(n2, d2)) => Ok(rational_add(n1, d1, n2, d2)),
+        _ => Err("Add requires two numbers"),
     }
 }
 
-fn subtract(left: Value, right: Value) -> Value {
-    match (try!(left), try!(right)) {
-        (Raw::Integer(a), Raw::Integer(b)) => new_integer(a - b),
-        (Raw::Rational(n, d), Raw::Integer(b)) => rational_add(n, d, -b, 1),
-        (Raw::Integer(a), Raw::Rational(n, d)) => rational_add(a, 1, -n, d),
-        (Raw::Rational(n1, d1), Raw::Rational(n2, d2)) => rational_add(n1, d1, -n2, d2)
+fn subtract(left: &Value, right: &Value) -> Result<Value, Error> {
+    match (left, right) {
+        (&Value::Integer(a), &Value::Integer(b)) => Ok(new_integer(a - b)),
+        (&Value::Rational(n, d), &Value::Integer(b)) => Ok(rational_add(n, d, -b, 1)),
+        (&Value::Integer(a), &Value::Rational(n, d)) => Ok(rational_add(a, 1, -n, d)),
+        (&Value::Rational(n1, d1), &Value::Rational(n2, d2)) => Ok(rational_add(n1, d1, -n2, d2)),
+        _ => Err("Subtract requires two numbers"),
     }
 }
 
-fn divide(left: Value, right: Value) -> Value {
-    match (try!(left), try!(right)) {
-        (Raw::Integer(a), Raw::Integer(b)) => new_rational(a, b),
-        (Raw::Rational(n, d), Raw::Integer(b)) => new_rational(n, d * b),
-        (Raw::Integer(a), Raw::Rational(n, d)) => new_rational(a * d, n),
-        (Raw::Rational(n1, d1), Raw::Rational(n2, d2)) => new_rational(n1 * d2, d1 * n2),
+fn divide(left: &Value, right: &Value) -> Result<Value, Error> {
+    match (left, right) {
+        (&Value::Integer(a), &Value::Integer(b)) => Ok(new_rational(a, b)),
+        (&Value::Rational(n, d), &Value::Integer(b)) => Ok(new_rational(n, d * b)),
+        (&Value::Integer(a), &Value::Rational(n, d)) => Ok(new_rational(a * d, n)),
+        (&Value::Rational(n1, d1), &Value::Rational(n2, d2)) => Ok(new_rational(n1 * d2, d1 * n2)),
+        _ => Err("Divide requires two numbers"),
     }
 }
 
-fn multiply(left: Value, right: Value) -> Value {
-    match (try!(left), try!(right)) {
-        (Raw::Integer(a), Raw::Integer(b)) => new_integer(a * b),
-        (Raw::Rational(n, d), Raw::Integer(b)) => new_rational(n * b, d),
-        (Raw::Integer(a), Raw::Rational(n, d)) => new_rational(a * n, d),
-        (Raw::Rational(n1, d1), Raw::Rational(n2, d2)) => new_rational(n1 * n2, d1 * d2),
+fn multiply(left: &Value, right: &Value) -> Result<Value, Error> {
+    match (left, right) {
+        (&Value::Integer(a), &Value::Integer(b)) => Ok(new_integer(a * b)),
+        (&Value::Rational(n, d), &Value::Integer(b)) => Ok(new_rational(n * b, d)),
+        (&Value::Integer(a), &Value::Rational(n, d)) => Ok(new_rational(a * n, d)),
+        (&Value::Rational(n1, d1), &Value::Rational(n2, d2)) => Ok(new_rational(n1 * n2, d1 * d2)),
+        _ => Err("Multiply requires two numbers"),
     }
 }
 
 fn test(a: Value, b: Value) {
-    println!("{:?} / {:?} = {:?}", a, b, divide(a, b));
-    println!("{:?} * {:?} = {:?}", a, b, multiply(a, b));
-    println!("{:?} + {:?} = {:?}", a, b, add(a, b));
-    println!("{:?} - {:?} = {:?}", a, b, subtract(a, b));
+    println!("{:?} / {:?} = {:?}", a, b, divide(&a, &b));
+    println!("{:?} * {:?} = {:?}", a, b, multiply(&a, &b));
+    println!("{:?} + {:?} = {:?}", a, b, add(&a, &b));
+    println!("{:?} - {:?} = {:?}", a, b, subtract(&a, &b));
 }
 
 fn main() {
@@ -112,4 +116,6 @@ fn main() {
     test(new_rational(2, 1), new_rational(1, 2));
     test(new_rational(1, 2), new_rational(1, 2));
     test(new_rational(2, 2), new_rational(2, 1));
+    test(Value::String(str::to_string("Input string")),
+         new_rational(2, 1));
 }
